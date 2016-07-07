cdee4ed190cda44fd1089319f0cf6214cce47a52
Andrei Volchenko <volchenko.andrei@gmail.com>
2016-07-07 20:47:57 +0300

    rewrite effect spec to be more verbose in docs

--- docs.md
+++ docs.md
@@ -3,6 +3,8 @@
      - [.be.iteration](#iteration-assertion-beiteration)
      - [.not.be.iteration](#iteration-assertion-notbeiteration)
    - [Effect assertion](#effect-assertion)
+     - [.be.effect](#effect-assertion-beeffect)
+     - [not.be.an.effect](#effect-assertion-notbeaneffect)
 <a name=""></a>
  
 <a name="iteration-assertion"></a>
@@ -28,5 +30,54 @@ expect(undefined).not.to.be.an.iteration;
 expect(42).not.to.be.an.iteration;
 expect(notIterationObject).not.to.be.an.iteration;
 notIterationObject.should.not.be.an.iteration;
+```
+
+<a name="effect-assertion"></a>
+# Effect assertion
+<a name="effect-assertion-beeffect"></a>
+## .be.effect
+does not fail if tested object is redux-saga effect.
+
+```js
+expect(take('ACTION')).to.be.an.effect;
+expect(takem('ACTION')).to.be.an.effect;
+expect(put({ type: 'ACTION' })).to.be.an.effect;
+expect(race([])).to.be.an.effect;
+expect(call(noop)).to.be.an.effect;
+expect(apply({}, noop)).to.be.an.effect;
+expect(cps(noop)).to.be.an.effect;
+expect(fork(noop)).to.be.an.effect;
+expect(spawn(noop)).to.be.an.effect;
+expect(join(mockTask)).to.be.an.effect;
+expect(cancel(mockTask)).to.be.an.effect;
+expect(actionChannel('ACTION')).to.be.an.effect;
+
+take('ACTION').should.be.an.effect;
+takem('ACTION').should.be.an.effect;
+put({ type: 'ACTION' }).should.be.an.effect;
+race([]).should.be.an.effect;
+call(noop).should.be.an.effect;
+apply({}, noop).should.be.an.effect;
+cps(noop).should.be.an.effect;
+fork(noop).should.be.an.effect;
+spawn(noop).should.be.an.effect;
+join(mockTask).should.be.an.effect;
+cancel(mockTask).should.be.an.effect;
+actionChannel('ACTION').should.be.an.effect;
+```
+
+<a name="effect-assertion-notbeaneffect"></a>
+## not.be.an.effect
+does not fail if tested object is not a redux-saga effect.
+
+```js
+const notEffectObject = { value: 42 };
+
+expect(null).not.to.be.an.effect;
+expect(undefined).not.to.be.an.effect;
+expect(42).not.to.be.an.effect;
+expect(notEffectObject).not.to.be.an.effect;
+
+notEffectObject.should.not.be.an.effect;
 ```
 
--- test/spec/2.effect.spec.js
+++ test/spec/2.effect.spec.js
@@ -1,33 +1,66 @@
 const Effects = require('redux-saga/effects');
+
+const { take } = Effects;
+const { takem } = Effects;
+const { put } = Effects;
+const { race } = Effects;
+const { call } = Effects;
+const { apply } = Effects;
+const { cps } = Effects;
+const { fork } = Effects;
+const { spawn } = Effects;
+const { join } = Effects;
+const { cancel } = Effects;
+const { actionChannel } = Effects;
+
 const { createMockTask } = require('redux-saga/utils');
 
 const mockTask = createMockTask();
 
-const argsForEffectCreator = {
-  take: ['ACTION'],
-  takem: ['ACTION'],
-  put: ['ACTION'],
-  race: [[]],
-  call: [noop],
-  apply: [{}, noop],
-  cps: [noop],
-  fork: [noop],
-  spawn: [noop],
-  join: [mockTask],
-  cancel: [mockTask],
-  actionChannel: ['ACTION'],
-};
-
 describe('Effect assertion', () => {
-  it('should detect effects correctly', () => {
-    expect(null).not.to.be.effect;
-    expect({ done: true, value: true }).not.to.be.effect;
+  describe('.be.effect', () => {
+    it('does not fail if tested object is redux-saga effect', () => {
+      expect(take('ACTION')).to.be.an.effect;
+      expect(takem('ACTION')).to.be.an.effect;
+      expect(put({ type: 'ACTION' })).to.be.an.effect;
+      expect(race([])).to.be.an.effect;
+      expect(call(noop)).to.be.an.effect;
+      expect(apply({}, noop)).to.be.an.effect;
+      expect(cps(noop)).to.be.an.effect;
+      expect(fork(noop)).to.be.an.effect;
+      expect(spawn(noop)).to.be.an.effect;
+      expect(join(mockTask)).to.be.an.effect;
+      expect(cancel(mockTask)).to.be.an.effect;
+      expect(actionChannel('ACTION')).to.be.an.effect;
 
-    Object.keys(Effects).forEach(effectName => {
-      const effectCreator = Effects[effectName];
-      const args = argsForEffectCreator[effectName];
 
-      expect(effectCreator.apply(null, args)).to.be.effect;
+      take('ACTION').should.be.an.effect;
+      takem('ACTION').should.be.an.effect;
+      put({ type: 'ACTION' }).should.be.an.effect;
+      race([]).should.be.an.effect;
+      call(noop).should.be.an.effect;
+      apply({}, noop).should.be.an.effect;
+      cps(noop).should.be.an.effect;
+      fork(noop).should.be.an.effect;
+      spawn(noop).should.be.an.effect;
+      join(mockTask).should.be.an.effect;
+      cancel(mockTask).should.be.an.effect;
+      actionChannel('ACTION').should.be.an.effect;
+    });
+  });
+
+  describe('not.be.an.effect', () => {
+    it('does not fail if tested object is not a redux-saga effect', () => {
+      const notEffectObject = { value: 42 };
+
+
+      expect(null).not.to.be.an.effect;
+      expect(undefined).not.to.be.an.effect;
+      expect(42).not.to.be.an.effect;
+      expect(notEffectObject).not.to.be.an.effect;
+
+
+      notEffectObject.should.not.be.an.effect;
     });
   });
 });
