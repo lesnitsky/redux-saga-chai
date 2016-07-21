7704ed9d6a49cbf14d7124367cca82a248635111
Andrei Volchenko <volchenko.andrei@gmail.com>
2016-07-21 20:17:35 +0300

    fix put spec

--- test/spec/4.put.spec.js
+++ test/spec/4.put.spec.js
@@ -17,7 +17,7 @@ describe('Effects', () => {
 
     describe('.not.be.putEffect', () => {
       it('does not fail if tested object is not `put` effect', () => {
-        const notPutEffectObject = take({ type: 'ACTION' });
+        const notPutEffectObject = take('ACTION');
 
 
         expect(null).not.to.be.an.putEffect;
@@ -37,9 +37,11 @@ describe('Effects', () => {
           yield put({ type: 'ACTION', payload: 42 });
         }
 
+
         const gen = testSaga();
         const first = gen.next();
         const second = gen.next();
+
 
         expect(first).to.put({ type: 'ACTION' });
         expect(second).to.put({ type: 'ACTION', payload: 42 });
@@ -47,23 +49,17 @@ describe('Effects', () => {
     });
 
     describe('.not.to.put(action)', () => {
-      it('does not fail if tested object is not `put` effect', () => {
-        function* testSaga() {
-          yield take('ACTION');
-        }
-
-        expect(testSaga().next()).not.to.put('ACTION');
-      });
-
       it('does not fail if tested object is `put` effect with incorrect action', () => {
         function* testSaga() {
           yield put({ type: 'ACTION' });
           yield put({ type: 'OTHER_ACTION', payload: 42 });
         }
 
+
         const gen = testSaga();
         const first = gen.next();
         const second = gen.next();
+
 
         expect(first).not.to.put({ type: 'OTHER_ACTION' });
         expect(second).not.to.put({ type: 'OTHER_ACTION' }); // no payload
